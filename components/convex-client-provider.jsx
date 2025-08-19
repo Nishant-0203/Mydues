8af75c3402267246c84c385626558170641019e8
Nishant Bhalla <nishantbhalla32@gmail.com>
2025-08-19 23:01:36 +0530

    Add Clerk authentication integration and update layout components

--- components/convex-client-provider.jsx
+++ components/convex-client-provider.jsx
@@ -1,9 +1,11 @@
 "use client";
-
+import { useAuth } from "@clerk/nextjs";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return <ConvexProvider client={convex}
+  useAuth={useAuth}
+  >{children}</ConvexProvider>;
 }
